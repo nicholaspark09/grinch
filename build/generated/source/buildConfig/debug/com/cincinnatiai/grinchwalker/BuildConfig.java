0acc38badaf4f7e78728cc7367fe282e8758742a
Nicholas Park <nicholaspark09@gmail.com>
2019-12-12 09:48:38 -0800

    Add Apache 2 license

--- build/generated/source/buildConfig/debug/com/cincinnatiai/grinchwalker/BuildConfig.java
+++ build/generated/source/buildConfig/debug/com/cincinnatiai/grinchwalker/BuildConfig.java
@@ -13,6 +13,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.cincinnatiai.grinchwalker";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 1800;
-  public static final String VERSION_NAME = "0.1.8";
+  public static final int VERSION_CODE = 1000;
+  public static final String VERSION_NAME = "0.1.0";
 }
